fc0b7d43cefb478507f0526b7da601bad42b5ec1
bosdo <bosdo@jfmedier.dk>
2021-10-18 21:15:18 +0200

    rm image before build

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker rm --force netspeed 
+[[ ! -z $(docker ps --filter name=netspeed -q) ]] && docker rm --force --volumes netspeed
 docker build --tag netspeed-metrics:latest . 
 docker run -d -p 3002:3002 --name netspeed netspeed-metrics:latest
