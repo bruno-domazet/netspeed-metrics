191cc1e0eec586e6f7d47c60f6ad378b52014936
bosdo <bosdo@jfmedier.dk>
2021-11-01 21:41:14 +0100

    explicit dns server

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 [[ ! -z $(docker ps --all --filter name=netspeed -q) ]] && docker rm --force --volumes netspeed
-docker build --tag netspeed-metrics:latest . 
-docker run -d -p 3002:3002 --name netspeed netspeed-metrics:latest
+docker build --tag netspeed-metrics:latest .
+DNS_IP=$(grep nameserver /etc/resolv.conf | cut -d ' ' -f2) 
+docker run --dns $DNS_IP -d -p 3002:3002 --name netspeed netspeed-metrics:latest
